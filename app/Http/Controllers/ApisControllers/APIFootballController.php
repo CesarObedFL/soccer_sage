291cb9dc4c39d601dca333f8bec1ee6c692c4df8
CesarObedFL <cesarobedfl@gmail.com>
2024-03-21 14:15:53 -0600

    [ADD] RAEDME.md

---
+++ app/Http/Controllers/ApisControllers/APIFootballController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ApisControllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class APIFootballController extends Controller
+{
+
+}
