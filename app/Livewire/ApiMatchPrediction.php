90b5a4f8c14128a76817e615a11b3953e1bed6a1
CesarObedFL <cesarobedfl@gmail.com>
2024-04-12 17:23:49 -0600

    [ADD] APIFootballController::get_api_prediction function to ApiMatchPrediction component

--- app/Livewire/ApiMatchPrediction.php
+++ app/Livewire/ApiMatchPrediction.php
@@ -4,14 +4,18 @@ namespace App\Livewire;
 
 use LivewireUI\Modal\ModalComponent;
 
+use App\Http\Controllers\ApisControllers\APIFootballController;
+
 class ApiMatchPrediction extends ModalComponent
 {
 
     public $fixture;
+    public $data;
 
     public function mount($fixture)
     {
         $this->fixture = $fixture;
+        $this->data = APIFootballController::get_api_prediction($this->fixture);
     }
 
     public function render()
