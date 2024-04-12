44f8e24fff77cda0c3273472dcb9f95c6a83b397
CesarObedFL <cesarobedfl@gmail.com>
2024-04-12 13:39:07 -0600

    [ADD] matches table v1

--- resources/views/livewire/matches.blade.php
+++ resources/views/livewire/matches.blade.php
@@ -28,21 +28,67 @@
                                         {{ $league['country'] }} &bull; {{ $league['season'] }} &bull; {{ $league['round'] }} 
                                     </p>
                                     <p class="mt-2">
+                                        <br>
                                         <ul>
-                                            @forelse ($league['matches'] as $match_index => $match)
-                                                <li wire:key="league-{{ $match_index }}">
-                                                    {{ $match['fixture_id'] }} &bull;
-                                                    {{ $match['date'] }} &bull;
-                                                    {{ $match['status']->long . ' - ' . 'min: ' . $match['status']->elapsed }} &bull;
-                                                    {{ $match['teams']->home->name }} <b><i>vs</i></b> {{ $match['teams']->away->name }} &bull; 
-                                                    {{ ($match['score']->fulltime->home) ? ($match['score']->fulltime->home) . ':' . ($match['score']->fulltime->away) : ($match['score']->halftime->home) . ':' . ($match['score']->halftime->home)}} &bull;
-                                                    is_home_team_saved: {{ ($match['is_home_team_saved']) ? 'true' : 'false' }} &bull; 
-                                                    is_away_team_saved: {{ ($match['is_away_team_saved']) ? 'true' : 'false' }} &bull; 
-                                                    bet_oportunity: {{ ($match['is_bet_oportunity']) ? 'true' : 'false' }}
-                                                </li>
-                                            @empty
-                                                <li>No Matches</li>
-                                            @endforelse
+                                            <table class="w-full">
+                                                <thead>
+                                                    <tr class="w-96 border-b border-gray-200">
+                                                        <th class="bg-emerald-400 !important text-center font-bold py-4 px-3">Match ID</th>
+                                                        <th class="bg-emerald-400 !important text-center font-bold py-4 px-3">Date</th>
+                                                        <th class="bg-emerald-400 !important text-center font-bold py-4 px-3">Status</th>
+                                                        <th class="bg-emerald-400 !important text-center font-bold py-4 px-3">Match</th>
+                                                        <th class="bg-emerald-400 !important text-center font-bold py-4 px-3">Score</th>
+                                                        <th class="bg-emerald-400 !important text-center font-bold py-4 px-3">Home Team Saved</th>
+                                                        <th class="bg-emerald-400 !important text-center font-bold py-4 px-3">Away Team Saved</th>
+                                                        <th class="bg-emerald-400 !important text-center font-bold py-4 px-3">Bet Oportunity</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody class="w-96">
+                                                @forelse ($league['matches'] as $match_index => $match)
+                                                
+                                                        <tr wire:key="league-{{ $match_index }}" class="w-96 hover:bg-gray-100 transition-colors group">
+                                                            <td class="py-4 px-4 text-center">{{ $match['fixture_id'] }}</td>
+                                                            <td class="py-4 px-4 text-center">{{ $match['date'] }}</td>
+                                                            <td class="py-4 px-4 text-center"> 
+                                                                @if ($match['status']->long == 'Match Finished') 
+                                                                    <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2 py-1.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400 uppercase">{{ $match['status']->long }}</span> 
+                                                                @elseif ($match['status']->long == 'Not Started') 
+                                                                    <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2 py-1.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400 uppercase">{{ $match['status']->long }}</span> 
+                                                                @else
+                                                                    <span class="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-yellow-300 border border-yellow-300">{{ $match['status']->long }}</span>
+                                                                @endif
+                                                                    {{ ' min: ' . $match['status']->elapsed }}
+                                                                </td>
+                                                            <td class="py-4 px-4 text-center">{{ $match['teams']->home->name }} <b><i>vs</i></b> {{ $match['teams']->away->name }}</td>
+                                                            <td class="py-4 px-4 text-center">{{ ($match['score']->fulltime->home) ? ($match['score']->fulltime->home) . ':' . ($match['score']->fulltime->away) : ($match['score']->halftime->home) . ':' . ($match['score']->halftime->home)}}</td>
+                                                            <td class="py-4 px-4 text-center">
+                                                                @if ($match['is_home_team_saved'])
+                                                                    <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2 py-1.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400 uppercase">true</span> 
+                                                                @else 
+                                                                    <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2 py-1.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400 uppercase">false</span> 
+                                                                @endif
+                                                            </td>
+                                                            <td class="py-4 px-4 text-center">
+                                                                @if ($match['is_away_team_saved']) 
+                                                                    <span class="bg-green-300 text-green-800 text-xs font-medium me-2 px-2 py-1.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400 uppercase">true</span> 
+                                                                @else 
+                                                                    <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2 py-1.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400 uppercase">false</span> 
+                                                                @endif
+                                                            </td>
+                                                            <td class="py-4 px-4 text-center">
+                                                                @if ($match['is_bet_oportunity'])
+                                                                    <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2 py-1.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400 uppercase">true</span> 
+                                                                @else 
+                                                                    <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2 py-1.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400 uppercase">false</span> 
+                                                                @endif
+                                                            </td>
+                                                        </tr>
+                                                    
+                                                @empty
+                                                    <tr><td class="py-4 px-4 text-center">No Matches</td></tr>
+                                                @endforelse
+                                                </tbody>
+                                            </table>
                                         </ul>
                                     </p>
                                 </div>
@@ -58,3 +104,5 @@
 
 
 </div>
+
+
