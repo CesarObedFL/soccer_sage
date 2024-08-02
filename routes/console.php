7e095dcd5d000e64ccd732a80d1c243a2790c5b3
CesarObedFL <cesarobedfl@gmail.com>
2024-08-02 14:04:17 -0600

    [FIX] execution permissions; chmod

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,13 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+use Illuminate\Support\Facades\Schedule;
+
+use App\Console\Commands\TestCreateUserCommand;
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+//Schedule::command('app:auto-create-user')->dailyAt('12:35');
